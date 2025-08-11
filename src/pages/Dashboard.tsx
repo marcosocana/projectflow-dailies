76aa4533fc4f714fc8c873e0b5cc313a2621002c
marcosocana <119530841+marcosocana@users.noreply.github.com>
2025-08-11 22:05:58 +0200

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -118,7 +118,7 @@ const Dashboard = () => {
 
 
       {/* Main Content */}
-      <main className="container mx-auto pt-0 py-[30px] px-0">
+      <main className="container mx-auto pt-0 py-[94px] px-0">
         {!currentProject ? (
           <div className="max-w-4xl mx-auto space-y-6">
             {/* Acceso con contraseña */}
