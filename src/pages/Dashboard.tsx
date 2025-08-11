d67f91e05e5d93ae6815f67d09ec52283cbce22c
marcosocana <119530841+marcosocana@users.noreply.github.com>
2025-08-11 21:56:33 +0200

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -168,7 +168,7 @@ const Dashboard = () => {
         ) : (
           <div className="min-h-screen">
             <AppSidebar currentProject={currentProject} />
-            <main className="ml-16 p-6 pt-[20px]">
+            <main className="ml-16 p-6 pt-[64pxpx]">
               <Routes>
                 <Route path="/" element={<Navigate to="home" replace />} />
                 <Route path="home" element={<HomeModule projectId={currentProject.id} />} />
